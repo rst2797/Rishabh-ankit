9cbccdc3c83a145fbf79925588ded16db9d5be2d
rst2797 <rishabhtomar.rishabh@gmail.com>
2024-03-10 22:50:16 +0530

    project 3 clock

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-// const height = document.querySelectorAll('input');
+let intervalId;
+const clock = document.getElementById('time-upt');
 
-// height.forEach((x)=> {
-//    return x.addEventListener('',function(e){
-//         console.log(e.target.value)
-//     })
-// })
+function startClock() {
+    intervalId = setInterval(function() {
+        const time = new Date().getSeconds();
+        clock.innerHTML = time;
+    }, 1000);
+}
 
+function stop(){
+    clearInterval(intervalId);
+}
 
-function calculateBmi () {
-    const height = document.getElementById('height').value;
-    const weight = document.getElementById('weight').value;
-    console.log(height,"height",weight)
-
-    if(isNaN(height) || isNaN(weight)){
-        alert("Provide correct number")
-        return;
-    }
-
-    const bmi = (weight / (height * height))*10000;
-
-    const result = document.getElementById('result');
-    result.textContent = `The BMI is ${bmi}`
+function reset(){
+    stop();
+    clock.textContent = '0';
 }
